39cb19166e83ef1828786b92cf4dfa8ea8013292
gmendes0 <gmendes230@gmail.com>
2019-10-26 00:00:28 -0300

    login and jwt

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,4 +1,7 @@
 const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const auth_config = require('../config/auth')
 
 module.exports = {
 
@@ -17,6 +20,29 @@ module.exports = {
     } catch (error) {
       
       return response.status(500).json({ error: `woops` })
+    }
+  },
+
+  async auth(request, response) {
+
+    try {
+      
+      const { email, password } = request.body
+
+      const user = await User.findOne({ email }).select('+password')
+
+      if (!user) return response.status(400).json({ error: `user doesn't exists` })
+
+      if (!await bcrypt.compare(password, user.password)) return response.status(400).json({ error: `invalid password` })
+
+      user.password = undefined
+
+      const token = jwt.sign({ id: user._id }, auth_config.secret, { expiresIn: 43200 })
+
+      response.status(200).json({ user, token })
+    } catch (error) {
+      
+      response.status(500).json({ error: `woops` })
     }
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,6 @@ const route = express.Router()
 const AuthController = require('./controllers/AuthController')
 
 route.post('/register', AuthController.register)
+route.post('/login', AuthController.auth)
 
 module.exports = route
