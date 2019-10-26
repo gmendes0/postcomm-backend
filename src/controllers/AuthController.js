98cc3e3d2f017b485af3e2fc8721a609ee7ddc3e
gmendes0 <gmendes230@gmail.com>
2019-10-25 23:37:20 -0300

    create user on database

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,11 +1,22 @@
 const User = require('../models/User')
 
 module.exports = {
+
   async register(request, response) {
+
     try {
-      return response.json({ message: "ok" })
+      
+      const { name, email, password } = request.body
+
+      if (await User.findOne({ email })) return response.status(400).json({ error: `user already exists` })
+
+      const user = await User.create({ name, email, password })
+
+      user.password = undefined
+      return response.status(201).json({ user })
     } catch (error) {
       
+      return response.status(500).json({ error: `woops` })
     }
   }
 }
--- src/models/User.js
+++ src/models/User.js
@@ -15,6 +15,7 @@ const UserSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+    select: false,
   },
   created_at: {
     type: Date,
